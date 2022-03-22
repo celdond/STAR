04b82653d6a25127f537a9c8af0284ac08d70d34
celdond <celdond@gmail.com>
2022-03-21 20:39:56 -0700

    work to get soup up and running on computer

--- Wishlist_Random.py
+++ Wishlist_Random.py
@@ -4,7 +4,8 @@ import urllib.request
 def main():
     u = input("Paste the url of your steam wishlist\n")
     wishlist = urllib.request.urlopen(u)
-    s = "g_rgWishlistData"
-    return 1
+    soup = BeautifulSoup(wishlist, 'html.parser')
+    app_ids = soup.find('"appid":')
+    print(app_ids)
 
 main()
