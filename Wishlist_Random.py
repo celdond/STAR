bc330578787e7482c8a069638d730a357c3d4010
celdond <celdond@gmail.com>
2022-05-11 22:01:00 -0700

    Changed logic to collect json data

--- Wishlist_Random.py
+++ Wishlist_Random.py
@@ -1,10 +1,24 @@
 import requests as r
-import parse as p
+import json
+import re
+import time
 
 def main():
     u = input("Paste the url of your steam wishlist\n")
-    wishlist = r.get(u)
-    l = list(p.findall('"appid":{appid},', wishlist.text))
-    print(l)
+    wishlist_data = json.loads(re.findall(r'g_strWishlistBaseURL = (".*?");', r.get(u).text)[0])
+
+    l = r.get(wishlist_data + 'wishlistdata/?p=0').json()
+    time.sleep(4)
+
+    index = 1
+    while 1:
+        to_update = r.get(wishlist_data + 'wishlistdata/?p=' + str(index))
+        update = to_update.json()
+        if not update:
+            break
+        l.update(update)
+        index += 1
+        time.sleep(4)
+
 
 main()
