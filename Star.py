e2e9e4c077286c40d114e55330a7cce43b22d5ac
celdond <celdond@gmail.com>
2022-07-20 01:01:34 -0700

    set-up for user table

--- Star.py
+++ Star.py
@@ -9,6 +9,14 @@ def setup_user_base():
 def build_user():
     new_database = input("Please Enter A Username.\n")
     user_base = sql3.connect('user_base.db')
+    cur_base = user_base.cursor()
+    cur_base.execute(""" CREATE TABLE IF NOT EXISTS users (
+                        name text PRIMARY KEY,
+                        password text,
+                    ); """)
+    new_password = input("Please Add A Password.\n")
+    cur_base.execute(" INSERT INTO users(name, password) SELECT ?, ?", (new_database, new_password,))
+    user_base.commit()
     user_base.close()
 
     conn = sql3.connect(new_database + '.db')
