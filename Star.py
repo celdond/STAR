2d8eafb2de8e289bfcb322c6cbde7f49078495df
celdond <celdond@gmail.com>
2022-07-21 21:31:14 -0700

    error in creating user's database found

--- Star.py
+++ Star.py
@@ -6,36 +6,37 @@ def build_user()->str:
     new_database = input("Please Enter A Username.\n")
     user_base = sql3.connect('user_base.db')
     cur_base = user_base.cursor()
-    cur_base.execute(""" CREATE TABLE IF NOT EXISTS users (
-                        name text PRIMARY KEY,
-                        password text,
-                    ); """)
+    cur_base.execute(""" CREATE TABLE IF NOT EXISTS users ( name text(255) PRIMARY KEY, password text(255)); """)
 
-    cur_base.execute("SELECT count(*) FROM users WHERE name = ?", (new_database))
-    if cursor.fetchall()[0] != 0:
+    cur_base.execute("SELECT count(*) FROM users WHERE name = ?", (new_database,))
+    data = cur_base.fetchall()[0]
+    if data[0] != 0:
         print("Username taken.")
         user_base.close()
-        return
+        return '/0'
 
     new_password = input("Please Add A Password.\n")
-    cur_base.execute(" INSERT INTO users(name, password) SELECT ?, ?", (new_database, new_password))
+    cur_base.execute(" INSERT INTO users(name, password) SELECT ?, ?", (new_database, new_password,))
     user_base.commit()
     user_base.close()
 
     this_dir = os.getcwd()
     database_path = os.path.join(this_dir, "Profiles", new_database)
     database_path = os.mkdir(database_path)
-    conn = sql3.connect(os.path.join(database_path, new_database) + '.db')
+    database_path = os.path.join(database_path, new_database, '.db')
+    conn = sql3.connect(database_path)
     conn.close()
 
     return new_database
 
-def sign_in():
+def sign_in()->str:
     user_name = input("Username: \n")
-    password = input("Password: \n");
+    password = input("Password: \n")
+    user_base = sql3.connect('user_base.db')
+
     cwd = os.getcwd()
-    
-    return
+    user_base.close()
+    return user_name
 
 def main():
     
