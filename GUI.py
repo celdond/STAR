1c74cee5df0fda7c597b2732573f66c9b22a1232
celdond <celdond@gmail.com>
2022-08-18 21:35:24 -0700

    added dashboard set-up and creation method to sign in

--- GUI.py
+++ GUI.py
@@ -2,6 +2,12 @@ import sys
 from PySide6.QtCore import QSize, Qt
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
+class dashboard(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Dashboard")
+
 class sign_in_window(QMainWindow):
     
     def __init__(self):
@@ -9,8 +15,12 @@ class sign_in_window(QMainWindow):
         self.setWindowTitle("STAR")
 
         sign_in_button = QPushButton("Sign In")
-
+        sign_in_button.clicked.connect(self.check_sign_in)
         self.setCentralWidget(sign_in_button)
+
+    def check_sign_in(self, checked):
+        d= dashboard()
+        d.show()
 
 star = QApplication(sys.argv)
 
