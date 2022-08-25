f1d6bf9d0dfed7c2d3ea2414dd3dd23ade60d6e6
celdond <celdond@gmail.com>
2022-08-25 01:40:54 -0700

    work done managing windows

--- GUI.py
+++ GUI.py
@@ -19,7 +19,9 @@ class dashboard(QMainWindow):
         tabs = QTabWidget()
         tabs.setTabPosition(QTabWidget.North)
 
-        tabs.addTab(home_page(), "Home")
+        self.home = home_page()
+
+        tabs.addTab(self.home, "Home")
         tabs.addTab(steam_wishlist_page(), "Steam")
         tabs.addTab(stats_page(), "Stats")
         self.setCentralWidget(tabs)
@@ -29,6 +31,18 @@ class home_page(QWidget):
     def __init__(self):
         super().__init__()
 
+        add_button = QPushButton("Add List")
+        add_button.clicked.connect(self.add_button)
+        wishlist_name = QLineEdit()
+        layout = QGridLayout()
+        layout.addWidget(wishlist_name, 0, 0)
+        layout.addWidget(add_button, 0, 1)
+
+        self.setLayout(layout)
+
+    def add_button(self):
+        pass
+
 class steam_wishlist_page(QWidget):
 
     def __init__(self):
@@ -36,10 +50,9 @@ class steam_wishlist_page(QWidget):
 
         rand_button = QPushButton("Random")
         rand_button.clicked.connect(self.random_button)
-        wishlist_name = QLineEdit()
+        
         layout = QGridLayout()
         layout.addWidget(rand_button, 0, 1)
-        layout.addWidget(wishlist_name, 0, 0)
 
         self.setLayout(layout)
     
@@ -65,6 +78,7 @@ class sign_in_window(QMainWindow):
         self.d = dashboard()
         self.d.show()
 
+def fetch_profile()
 
 def main():
 
