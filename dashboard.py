daf76524a2ecae64f51b42f3944c40fbceb10db6
celdond <celdond@gmail.com>
2022-08-27 01:55:41 -0700

    added platform selection and fixed window closing

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,7 @@ from PySide6.QtWidgets import (
     QWidget,
     QGridLayout,
     QLineEdit,
+    QComboBox,
 )
 
 class dashboard(QMainWindow):
@@ -35,15 +36,24 @@ class home_page(QWidget):
 
         add_button = QPushButton("Add List")
         add_button.clicked.connect(self.add_button)
+
+        platform_selection = QComboBox()
+        platform_selection.addItems(["Steam"])
+        platform_selection.currentIndexChanged.connect(self.platform_selected)
+
         wishlist_name = QLineEdit()
         layout = QGridLayout()
         layout.addWidget(wishlist_name, 0, 0)
         layout.addWidget(add_button, 0, 1)
+        layout.addWidget(platform_selection, 0, 2)
 
         self.setLayout(layout)
 
     def add_button(self):
-        pass
+        return
+
+    def platform_selected(self, i):
+        return
 
 class steam_wishlist_page(QWidget):
 
