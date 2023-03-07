a952780ffb4721dd4e4a94042a6a953306dc1db7
celdond <celdond@gmail.com>
2023-03-06 23:03:06 -0800

    set tabs up in dashboard

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.lang import Builder
 
-class Dashboard(Widget):
-    pass
+Builder.load_file('source_new/ui/dashboard.kv')
+
+class Dashboard(TabbedPanel):
+
+    def __init__(self, **kwargs):
+        super(Dashboard, self).__init__(**kwargs)
 
 class StarApp(App):
     def build(self):
