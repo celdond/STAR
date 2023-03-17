a2cab086cdf9775314f78b10180e8b295cd888b5
celdond <celdond@gmail.com>
2023-03-16 21:21:59 -0700

    buttons set in profile select screen

--- source/gui/profile.py
+++ source/gui/profile.py
@@ -13,6 +13,12 @@ class profile_window(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Profile Selection")
-        sign_in_button = QPushButton("Select Profile")
+        select_button = QPushButton("Select Profile")
         ## sign_in_button.clicked.connect(self.check_sign_in)
-        profile_button = QPushButton("Create Profile")
+        create_button = QPushButton("Create Profile")
+
+        layout = QGridLayout()
+        layout.addWidget(select_button, 1, 0)
+        layout.addWidget(create_button, 1, 1)
+
+        self.setLayout(layout)
