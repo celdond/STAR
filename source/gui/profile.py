c37c0886a44f325db71895e53de400d874fa1ba2
celdond <celdond@gmail.com>
2023-03-27 15:57:35 -0700

    merged all profile menus into one

--- source/gui/profile.py
+++ source/gui/profile.py
@@ -8,33 +8,7 @@ from PySide6.QtWidgets import (
     QLabel,
 )
 
-
 class profile_window(QWidget):
-    
-    def __init__(self, dashboard_status):
-        super().__init__()
-        self.dashboard_status = dashboard_status
-        self.setWindowTitle("Profile Selection")
-        select_button = QPushButton("Select Profile")
-        create_button = QPushButton("Create Profile")
-        create_button.clicked.connect(self.open_select_window)
-        create_button.clicked.connect(self.open_create_window)
-
-        layout = QGridLayout()
-        layout.addWidget(select_button, 1, 0)
-        layout.addWidget(create_button, 1, 1)
-
-        self.setLayout(layout)
-
-    def open_create_window(self):
-        self.dashboard_status.creation.show()
-        self.hide()
-
-    def open_create_window(self):
-        self.dashboard_status.selection.show()
-        self.hide()
-
-class create_profile(QWidget):
 
     def __init__(self, dashboard_status):
         super().__init__()
@@ -42,6 +16,8 @@ class create_profile(QWidget):
 
         profile_button = QPushButton("Create Profile")
         profile_button.clicked.connect(self.create)
+        sign_in_button = QPushButton("Sign In")
+        sign_in_button.clicked.connect(self.check_sign_in)
         self.entered_username = QLineEdit()
         self.entered_password = QLineEdit()
 
@@ -51,6 +27,7 @@ class create_profile(QWidget):
         layout.addWidget(self.entered_username, 0, 1)
         layout.addWidget(self.entered_password, 1, 1)
         layout.addWidget(profile_button, 2, 0)
+        layout.addWidget(sign_in_button, 2, 1)
 
         self.setLayout(layout)
 
@@ -71,39 +48,9 @@ class create_profile(QWidget):
         self.dashboard_status.profile_menu.addAction(self.dashboard_status.clear_profile)
         return
     
-class select_profile(QWidget):
-
-    def __init__(self, dashboard_status):
-        super().__init__()
-        self.dashboard_status = dashboard_status
-
-        profile_button = QPushButton("Log In")
-        profile_button.clicked.connect(self.create)
-        self.entered_username = QLineEdit()
-        self.entered_password = QLineEdit()
-
-        layout = QGridLayout()
-        layout.addWidget(QLabel("Username"), 0, 0)
-        layout.addWidget(QLabel("Password"), 1, 0)
-        layout.addWidget(self.entered_username, 0, 1)
-        layout.addWidget(self.entered_password, 1, 1)
-        layout.addWidget(profile_button, 2, 0)
-
-        self.setLayout(layout)
-
-    def log_in(self):
-        if '/' in self.entered_username.text():
-            illegal_dialogue = dialogues.sign_in_failure_dialogue("Failure", "Character '/' is not allowed.")
-            illegal_dialogue.exec()
-            return
-        taken = userbase.build_user(self.entered_username.text(), self.entered_password.text())
-
-        if taken == '/0':
-            fail_dialogue = dialogues.sign_in_failure_dialogue("Name taken.")
+    def check_sign_in(self):
+        if userbase.fetch_profile(self.entered_username.text(), self.entered_password.text()) == '/0':
+            fail_dialogue = dialogues.sign_in_failure_dialogue("Failure", "Username or password is incorrect.")
             fail_dialogue.exec()
             return
-        
-        legal_dialogue = dialogues.sign_in_failure_dialogue("Profile Created", "Profile Successfully Created!")
-        legal_dialogue.exec()
-        self.dashboard_status.profile_menu.addAction(self.dashboard_status.clear_profile)
-        return
+        self.close()
