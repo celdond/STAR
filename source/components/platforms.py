8f3d27d6ed9ef08c8591b196650cee9e6aaec0f0
celdond <celdond@gmail.com>
2023-03-28 18:36:23 -0700

    platform selection added in platforms

--- source/components/platforms.py
+++ source/components/platforms.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import (
+    QComboBox,
     QPushButton,
     QWidget,
     QGridLayout,
@@ -11,3 +12,16 @@ class platforms_window(QWidget):
     def __init__(self, dashboard_status):
         super().__init__()
         self.dashboard_status = dashboard_status
+        self.platform = None
+        platform_selection = QComboBox()
+        platform_selection.addItems(["None", "Steam"])
+
+        platform_selection.currentTextChanged.connect(self.platform_changed)
+
+        layout = QGridLayout()
+        layout.addWidget(platform_selection, 0, 0)
+        self.setLayout(layout)
+
+    def platform_change(self, s):
+        self.platform = s
+        return
