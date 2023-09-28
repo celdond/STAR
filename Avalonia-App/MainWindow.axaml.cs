83cffaeb6edb08ed6f8d39c6eb112a49883869b1
celdond <celdond@gmail.com>
2023-09-28 00:03:22 -0700

    start layout of random selection page

--- Avalonia-App/MainWindow.axaml.cs
+++ Avalonia-App/MainWindow.axaml.cs
@@ -1,11 +1,20 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
 
-namespace STAR;
-
-public partial class MainWindow : Window
+namespace STAR
 {
-    public MainWindow()
+
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Random_Select(object sender, RoutedEventArgs e)
+        {
+        }
     }
+
 }
