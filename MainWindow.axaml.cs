216e7888260b2fd81ec617e5a34fddc8933eff36
celdond <celdond@gmail.com>
2025-04-09 01:11:06 -0700

    Revert "removed c sharp, moving to PyO3 Kivy"
    This reverts commit 6ff5628e93dc9b7d8da6c6c43215b3ede22197c8.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace STAR
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Random_Select(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+
+}
